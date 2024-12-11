fd8628bbe6791a114beb98ddc25692edc5600265
juliasubbotina <yulia.subbotina@ucd.ie>
2024-12-11 21:20:55 +0000

    ver.0.0.0

---
+++ scaleup/configs/.ipynb_checkpoints/run_ua-checkpoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh 
+screen -a -S 6.8_ag111 
+ ./UnitedAtom --config-file=6.8_ag111.config 2>&1 
+
+
+
+screen -a -S 7.3_ag111 
+ ./UnitedAtom --config-file=7.3_ag111.config 2>&1 
+
+
+
+screen -a -S 8.5_ag111 
+ ./UnitedAtom --config-file=8.5_ag111.config 2>&1 
+
+
+
